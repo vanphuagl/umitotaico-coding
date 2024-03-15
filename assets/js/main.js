565a89385feeb689ee15310ad640d6767e7b23c8
unknown <nguyen_van_phu@allgrow-labo.jp>
2024-03-15 10:41:17 +0700

    coding menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 "use strict";
 
+/* ---------------------- add event on multiple element --------------------- */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  if (elements) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
+};
+
 /* --------------------------- resize mobile 100vh -------------------------- */
 
 const appHeight = () => {
@@ -67,3 +77,26 @@ const jsFade = document.querySelectorAll(".js-fade");
     });
   });
 });
+
+/* ----------------------------- menu show/hide ----------------------------- */
+
+const [menuToggler, menuLinks, menu] = [
+  document.querySelectorAll("[data-menu-toggler]"),
+  document.querySelectorAll("[data-menu-link]"),
+  document.querySelector("[data-menu]"),
+];
+
+const toggleMenu = () => {
+  menu.classList.toggle("--active");
+  reservation.classList.toggle("--menu-active");
+  document.body.classList.toggle("--disable-scroll");
+};
+
+addEventOnElements(menuToggler, "click", toggleMenu);
+
+const closeMenu = () => {
+  menu.classList.remove("--active");
+  document.body.classList.remove("--disable-scroll");
+};
+
+addEventOnElements(menuLinks, "click", closeMenu);
