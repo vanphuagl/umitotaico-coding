915c33c4e9f596df0aab384d92e221a62cca7c58
unknown <nguyen_van_phu@allgrow-labo.jp>
2024-03-25 16:18:07 +0700

    update something

--- assets/js/main.js
+++ assets/js/main.js
@@ -297,3 +297,17 @@ window.addEventListener(
   },
   false
 );
+
+(function () {
+  if (typeof EventTarget !== "undefined") {
+    let func = EventTarget.prototype.addEventListener;
+    EventTarget.prototype.addEventListener = function (type, fn, capture) {
+      this.func = func;
+      if (typeof capture !== "boolean") {
+        capture = capture || {};
+        capture.passive = false;
+      }
+      this.func(type, fn, capture);
+    };
+  }
+})();
